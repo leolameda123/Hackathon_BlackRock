7baf13037abd0b4714155148c7a69a78dff362e0
ig <leoisaac.lameda@gmail.com>
2025-07-19 11:40:02 -0600

    Fist endpoint

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask
+from flask import Flask, request
+from collections import OrderedDict
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -13,7 +14,24 @@ def create_app(test_config=None):
         pass
 
     @app.route("/")
-    def hello():
+    def test():
         return "I'm Alive"
 
+    @app.post('/blackrock/challenge/v1/transactions:parse')
+    def ParseTransactions():
+        data = request.get_json()
+        GetRemanents(data["Gastos"])
+        return data
+        
     return app
+
+def GetRemanents(data):
+    for entry in data:
+        amount = entry["amount"]
+        ceil = (amount//100 + 1) * 100
+        remanent = int((ceil - amount)*100)/100
+
+        entry["amount"] = float(amount)
+        entry["ceiling"] = float(ceil)
+        entry["remanent"] = remanent
+    return
