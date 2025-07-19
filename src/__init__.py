554084bbb8ff889d8e4f85c15181d705b21d4575
ig <leoisaac.lameda@gmail.com>
2025-07-19 13:36:08 -0600

    functions secondendpoint

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, request
-from collections import OrderedDict
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -17,21 +16,76 @@ def create_app(test_config=None):
     def test():
         return "I'm Alive"
 
-    @app.post('/blackrock/challenge/v1/transactions:parse')
+    @app.get('/blackrock/challenge/v1/transactions:parse')
     def ParseTransactions():
         data = request.get_json()
-        GetRemanents(data["Gastos"])
+        GetRemanents(data)
         return data
+        
+    return app
+
+    @app.get('/blackrock/challenge/v1/transactions:validator')
+    def ParseTransactions():
+        data = request.get_json()
+        return Validator(data)
         
     return app
 
 def GetRemanents(data):
     for entry in data:
         amount = entry["amount"]
-        ceil = (amount//100 + 1) * 100
-        remanent = int((ceil - amount)*100)/100
+        if amount > 0:
+            entry["amount"] = float(amount)
+            entry["ceiling"], entry["remanent"] = CalculateRemanents(amount)
 
-        entry["amount"] = float(amount)
-        entry["ceiling"] = float(ceil)
-        entry["remanent"] = remanent
+        else:
+            entry["ceiling"] = 0
+            entry["remanent"] = 0
     return
+
+def CalculateRemanents(amount):
+    ceil = (amount//100 + 1) * 100
+    remanent = int((ceil - amount)*100)/100
+    return (float(ceil), float(remanent))
+
+def Validator(data):
+    hs = set()
+    valid = []
+    invalid = []
+
+    wage = data["wage"]
+    transactions = data["transactions"]
+
+    for entry in transactions:
+        temp = (entry["date"], entry["amount"], entry["ceiling"], entry["remanent"])
+        hs.append(temp)
+        if DuplicateValidator != True:
+            invalid.append(entry)
+            continue
+        if ZeroValidator != True:
+            invalid.append(entry)
+            continue
+        if PositiveValidator != True:
+            invalid.append(entry)
+            continue
+        if RemanentValidator != True:
+            invalid.append(entry)
+            continue
+        
+        valid.append(entry)
+
+    return {"valid": valid, "invalid": invalid} 
+
+
+
+def DuplicateValidator(entry, hs):
+    return True if entry not in hs else "Duplicate Transaction"
+
+def ZeroValidator(entry):
+    return True if entry["amount"] != 0 else "Zero amounts are not allowed"
+
+def PositiveValidator(entry):
+    return True if entry["amount"] > 0 else "Negative amounts are not allowed"
+
+def RemanentValidator(entry):
+    return True if (entry["ceiling"], entry["remanent"]) == CalculateRemanents(entry["amount"]) else "Error in remanent values"
